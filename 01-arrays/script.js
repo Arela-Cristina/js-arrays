5faa9f773c7004ed163859aee0dbf2e7314ab7a0
Arela-Cristina <instagrandine@gmail.com>
2024-10-16 14:58:43 +0200

    third exercise done

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,17 +10,18 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers[3];
+const fourthTeacher = teachers[3]; //stringa
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-let index = teachers.indexOf('Lewis')
+let index = teachers.indexOf('Lewis') //number
 console.log(index);
 teachers.splice(index, 1, 'Patrick')
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop(); //stringa
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
